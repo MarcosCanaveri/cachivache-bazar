7a5f347b119f315149b1207f238ae7286382b077
MarcosCanaveri <marcoscanaveri@gmail.com>
2026-02-22 18:34:57 -0300

    public

---
+++ routers - handlebars/static/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import apiRouter from "./routes/index.js";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${process.cwd()}/src/public`));
+
+
+app.use('/api', apiRouter);
+
+
+app.listen(8080, () => {
+  console.log('Servidor escuchando en el puerto 8080');
+});
